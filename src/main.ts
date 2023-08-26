9bc8d14df2738e63d64b6a18e83dd2f79cea1441
Marc <info@marcamoros.io>
2023-08-26 15:48:55 +0200

    fix(main): adds type to greeter function param

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,12 @@ export enum Delays {
  * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
  * @returns {Promise<string>}
  */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
+function delayedHello(name: string, delay: number = Delays.Medium): Promise<string> {
   return new Promise((resolve: (value?: string) => void) =>
     setTimeout(() => resolve(`Hello, ${name}`), delay),
   );
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
+export async function greeter(name: string) {
   return await delayedHello(name, Delays.Long);
 }
